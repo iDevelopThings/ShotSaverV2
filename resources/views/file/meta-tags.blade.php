9d5c4d8163ffb0ea810a3cf71f27f473f43d0481
Sam Parton <Sam@iDevelopThings.com>
2019-09-21 17:04:51 +0100

    Update meta tags to fix bug

--- resources/views/file/meta-tags.blade.php
+++ resources/views/file/meta-tags.blade.php
@@ -5,10 +5,10 @@
     <meta property="og:image:type" content="{{$file->mime_type}}">
     <meta property="og:type" content="image">
 
-    @if($dimensions != null)
-        <meta property="og:image:width" content="{{$dimensions['width']}}" />
-        <meta property="og:image:height" content="{{$dimensions['height']}}" />
-    @endif
+
+    <meta property="og:image:width" content="1920" />
+    <meta property="og:image:height" content="1080" />
+
 
     {{--@elseif($file->type == 'video')
 
