9d0da5ad4f4f593651255c0cf44af7a2d597ebed
Sam Parton <tony8tr@hotmail.com>
2020-09-11 11:04:40 +0100

    - Used incorrect structure for outputting dimensions

--- resources/views/file/meta-tags.blade.php
+++ resources/views/file/meta-tags.blade.php
@@ -7,8 +7,8 @@
     <meta property="og:type" content="image">
 
 
-    <meta property="og:image:width" content="{{$file->meta['hd']['dimensions'][0]}}" />
-    <meta property="og:image:height" content="{{$file->meta['hd']['dimensions'][1]}}" />
+    <meta property="og:image:width" content="{{$file->meta['dimensions']['hd'][0]}}" />
+    <meta property="og:image:height" content="{{$file->meta['dimensions']['hd'][1]}}" />
     <meta content="article" property="og:type" />
 @endif
 
@@ -25,21 +25,21 @@
     <meta property="og:image" content="{{$file->file('thumb')}}" />
     <meta property="og:image:secure_url" content="{{$file->file('thumb')}}" />
     <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="{{$file->meta['hd']['dimensions'][0]}}">
-    <meta property="og:image:height" content="{{$file->meta['hd']['dimensions'][1]}}">
+    <meta property="og:image:width" content="{{$file->meta['dimensions']['hd'][0]}}">
+    <meta property="og:image:height" content="{{$file->meta['dimensions']['hd'][1]}}">
 
     <meta property="og:updated_time" content="{{$file->created_at->toAtomString() }}" />
     <meta property="og:video" content="{{$file->file('hd')}}">
     <meta property="og:video:url" content="{{$file->file('hd')}}">
     <meta property="og:video:secure_url" content="{{$file->file('hd')}}">
     <meta property="og:video:type" content="video/mp4">
-    <meta property="og:video:width" content="{{$file->meta['hd']['dimensions'][0]}}">
-    <meta property="og:video:height" content="{{$file->meta['hd']['dimensions'][1]}}">
+    <meta property="og:video:width" content="{{$file->meta['dimensions']['hd'][0]}}">
+    <meta property="og:video:height" content="{{$file->meta['dimensions']['hd'][1]}}">
     <meta name="twitter:card" content="player">
     <meta name="twitter:site" content="@ShotSaver">
     <meta name="twitter:image" content="{{$file->file('thumb')}}">
-    <meta name="twitter:player:width" content="{{$file->meta['hd']['dimensions'][0]}}">
-    <meta name="twitter:player:height" content="{{$file->meta['hd']['dimensions'][1]}}">
+    <meta name="twitter:player:width" content="{{$file->meta['dimensions']['hd'][0]}}">
+    <meta name="twitter:player:height" content="{{$file->meta['dimensions']['hd'][1]}}">
     {{--<meta name="twitter:player" content="{{route('file-twitter', ['file' => $file->name])}}">--}}
     <meta name="twitter:player:stream" content="{{$file->file('hd')}}">
     <meta name="twitter:player:stream:content_type" content="video/mp4">
