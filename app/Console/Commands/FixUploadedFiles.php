151a34df571af1220e210c236bb592120112f222
Sam Parton <tony8tr@hotmail.com>
2020-09-13 19:37:08 +0100

    - Add progress bar to fix files command

--- app/Console/Commands/FixUploadedFiles.php
+++ app/Console/Commands/FixUploadedFiles.php
@@ -7,44 +7,51 @@ use Illuminate\Console\Command;
 
 class FixUploadedFiles extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'files:fix';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'files:fix';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'This command will set the file visibility correctly for all previously uploaded files';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'This command will set the file visibility correctly for all previously uploaded files';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        File::orderBy('id', 'desc')->chunk(100, function($files) {
-        	foreach($files as $file) {
-				if($file->private)
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+
+		$bar = $this->output->createProgressBar(File::count());
+		$bar->start();
+
+		File::orderBy('id', 'desc')->chunk(100, function ($files) use ($bar) {
+			foreach ($files as $file) {
+				if ($file->private) {
 					$file->setPrivate();
-				else
+				} else {
 					$file->setPublic();
-	        }
-        });
-    }
+				}
+				$bar->advance();
+			}
+
+		});
+	}
 }
